874be63d29415691c1e5ad8b0a8541f94c7ecf27
rstacks <stacksrob910@gmail.com>
2025-04-28 13:28:08 -0500

    Played around with Flask

--- app.py
+++ app.py
@@ -2,6 +2,10 @@ from flask import Flask
 
 app = Flask(__name__)
 
-@app.route("/")
+@app.get("/")
 def hello_world():
-  return "<h1>Howdy, world!</h1>"
+  return {"balls": 2}
+
+@app.get("/name/<int:username>")
+def show_name(username):
+  return f"Hey there, {username}"
