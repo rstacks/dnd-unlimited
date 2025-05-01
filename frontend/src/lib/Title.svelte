b20c081ba10b32ea3eb0095be0fcd5279bce74f3
rstacks <stacksrob910@gmail.com>
2025-05-01 17:11:23 -0500

    Updated global styles with background and border colors

--- frontend/src/lib/Title.svelte
+++ frontend/src/lib/Title.svelte
@@ -1,4 +1,4 @@
-<header>
+<header class="card">
   <img src="favicon.svg" alt="DnD Unlimited Logo">
   <h1>DnD Unlimited</h1>
 </header>
@@ -8,7 +8,18 @@
     display: flex;
     align-items: center;
     justify-content: center;
+    margin: auto;
     margin-bottom: 1em;
+    margin-top: 1em;
+    max-width: 25em;
+    width: 90%;
+    padding: 0.5em;
+  }
+
+  @media (max-width: 500px) {
+    header {
+      font-size: 0.9em;
+    }
   }
 
   img {
--- frontend/src/routes/+page.svelte
+++ frontend/src/routes/+page.svelte
@@ -1,3 +1,8 @@
+<script lang="ts">
+  import Title from "$lib/Title.svelte";
+</script>
+
+<Title />
 <h1>Welcome to SvelteKit</h1>
 <p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
 <button class="success">balls</button>
