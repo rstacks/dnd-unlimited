f3f2899a69b3f7f1741031617ebf266464cbc014
rstacks <stacksrob910@gmail.com>
2025-05-02 21:31:09 -0500

    Made auth occur on a single page

--- frontend/src/routes/login/+page.svelte
+++ frontend/src/routes/login/+page.svelte
@@ -4,8 +4,79 @@
 
 <script lang="ts">
   import Title from "$lib/components/Title.svelte";
-  import AuthForm from "$lib/components/AuthForm.svelte";
+  
+  let formtype: "login" | "register" = $state("login");
+
+  function switchFormtype(currentFormtype: "login" | "register"): void {
+    if (currentFormtype === "login") {
+      formtype = "register";
+    } else if (currentFormtype === "register") {
+      formtype = "login";
+    }
+  }
 </script>
 
 <Title />
-<AuthForm />
+
+<article class="card">
+  {#if formtype === "login"}
+    <h2>Log In</h2> 
+  {:else if formtype === "register"}
+    <h2>Register</h2>
+  {/if}
+  
+  <form method="POST" action="?/login">
+    <label>
+      <input type="tel" inputmode="numeric" name="phone" placeholder="Phone Number"
+        class="phone-input" autocomplete="off">
+    </label>
+
+    {#if formtype === "register"}
+      <label>
+        <input type="text" name="user-name" placeholder="Name"
+          class="phone-input" autocomplete="off">
+      </label>
+    {/if}
+    
+    {#if formtype === "login"}
+      <input type="submit" class="login" value="Log In">
+    {:else if formtype === "register"}
+      <input type="submit" class="login" value="Register" formaction="?/register">
+    {/if}
+  </form>
+  
+  <button class="pseudo register" onclick="{() => { switchFormtype(formtype) }}">
+    {#if formtype === "login"}
+      No account yet? Register
+    {:else if formtype === "register"}
+      Returning user? Login
+    {/if}
+  </button>
+</article>
+
+<style>
+  article {
+    max-width: 25em;
+    width: 75%;
+    justify-content: center;
+    margin: auto;
+    text-align: center;
+  }
+
+  .login {
+    display: block;
+    margin: auto;
+    margin-bottom: 0.5em;
+  }
+
+  .register {
+    width: fit-content;
+    margin: auto;
+    margin-bottom: 0.5em;
+  }
+
+  .phone-input {
+    width: 75%;
+    margin-bottom: 1em;
+  }
+</style>
