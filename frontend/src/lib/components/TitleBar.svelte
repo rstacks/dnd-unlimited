fddad38623cecadf6ae9472daa1b26dbd2598b42
rstacks <stacksrob910@gmail.com>
2025-05-03 18:20:27 -0500

    Got title bar frontend done

---
+++ frontend/src/lib/components/TitleBar.svelte
@@ -0,0 +1,54 @@
+<header class="flex five">
+  <div class="logo off-fifth three-fifth">
+    <img src="favicon.svg" alt="DnD Unlimited Logo">
+    <h1>DnD Unlimited</h1>
+  </div>
+  <div class="view-account">
+    <button class="pseudo">
+      <img src="account-icon-black.svg" alt="View Account Button">
+    </button>
+  </div>
+</header>
+
+<style>
+  header {
+    padding-top: 0.2em;
+    background-color: white;
+    border-style: solid;
+    border-color: rgb(118, 155, 255);
+    border-left: none;
+    border-right: none;
+    border-top: none;
+  }
+
+  .view-account {
+    margin: 0;
+    padding: 0;
+    display: flex;
+    justify-content: end;
+    align-items: center;
+  }
+
+  .view-account button {
+    margin: 0;
+    margin-right: 0.5em;
+    padding: 0.4em;
+    height: fit-content;
+  }
+
+  .view-account button img {
+    width: 1.5em;
+  }
+
+  .logo {
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    font-size: 0.6em;
+  }
+
+  .logo img {
+    width: 4em;
+    margin-right: 1em;
+  }
+</style>
--- frontend/src/routes/dashboard/+page.svelte
+++ frontend/src/routes/dashboard/+page.svelte
@@ -3,9 +3,12 @@
 </svelte:head>
 
 <script lang="ts">
+  import TitleBar from "$lib/components/TitleBar.svelte";
   import type { PageProps } from "./$types";
 
   let { data }: PageProps = $props();
 </script>
+
+<TitleBar />
 
 <h2>Welcome, {data.name}!</h2>
