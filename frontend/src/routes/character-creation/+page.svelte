0351b729900ef0dd1d245f249ea1812206acba44
rstacks <stacksrob910@gmail.com>
2025-05-04 16:57:13 -0500

    Character creation spawns modal before sending you back to dash

--- frontend/src/routes/character-creation/+page.svelte
+++ frontend/src/routes/character-creation/+page.svelte
@@ -1,3 +1,7 @@
+<svelte:head>
+  <title>Character Creation | DnD Unlimited</title>
+</svelte:head>
+
 <script lang="ts">
   // TODO: reset form inputs when close button pressed
   // function closeCharacterCreation(): void {
@@ -8,18 +12,36 @@
   // }
 </script>
   
-<article class="card" id="char-creation-screen">
-  <header>
-    <a class="back-button pseudo button" href="/dashboard">
+<article class="card char-creation-screen" id="char-creation-screen">
+  <header class="char-creation-header">
+    <label for="leave-modal" class="back-button pseudo button">
       <img src="back-icon.svg" alt="Back to Dashboard Button">
-    </a>
+    </label>
     <h3>Character Creation</h3>
   </header>
   <p>test</p>
 </article>
 
+<div class="modal">
+  <input type="checkbox" id="leave-modal">
+  <label for="leave-modal" class="overlay"></label>
+  <article>
+    <header>
+      <h3>Confirm Exit</h3>
+      <label for="leave-modal" class="close">&times;</label>
+    </header>
+    <section class="content">
+      Are you sure you want to leave?
+      <strong>All of your progress will be lost!</strong>
+    </section>
+    <footer>
+      <a class="button dangerous exit-button" href="/dashboard">Leave Character Creation</a>
+    </footer>
+  </article>
+</div>
+
 <style>
-  article {
+  .char-creation-screen {
     position: fixed;
     height: 100%;
     width: 100%;
@@ -27,7 +49,7 @@
     border-radius: 0;
   }
 
-  header {
+  .char-creation-header {
     display: flex;
     align-items: center;
   }
@@ -36,6 +58,16 @@
     display: flex;
     padding: 0.25em;
     margin-right: 1em;
+  }
+
+  footer {
+    width: fit-content;
+    margin: auto;
+  }
+
+  .exit-button {
+    margin: auto;
+    margin-bottom: 1em;
   }
 </style>
   
