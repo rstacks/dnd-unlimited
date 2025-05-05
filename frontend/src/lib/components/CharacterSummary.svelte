025a7adb06e757fa2e5e11cb42204debf4a18a18
rstacks <stacksrob910@gmail.com>
2025-05-05 17:52:41 -0500

    Finalized styles for character summary card

--- frontend/src/lib/components/CharacterSummary.svelte
+++ frontend/src/lib/components/CharacterSummary.svelte
@@ -2,17 +2,48 @@
   <header>
     <h4>Character Summary</h4>
   </header>
-  <p>content</p>
+  <p><strong>Name:</strong> Roxy</p>
+  <p><strong>Melee Weapon:</strong> Sword</p>
+  <p><strong>Ranged Weapon:</strong> Gun</p>
+  <p><strong>Class:</strong> Sorcerer</p>
+  <p><strong>Ability Scores:</strong></p>
+  <div class="ability-scores">
+    <div>
+      <p><strong>STR:</strong> 14</p>
+      <p><strong>DEX:</strong> 14</p>
+      <p><strong>CON:</strong> 14</p>
+    </div>
+    <div>
+      <p><strong>INT:</strong> 14</p>
+      <p><strong>WIS:</strong> 14</p>
+      <p><strong>CHA:</strong> 14</p>
+    </div>
+  </div>
 </article>
 
 <style>
   article {
     width: 20em;
     margin: auto;
+    font-size: 0.9em;
   }
 
   header {
     display: flex;
     align-items: center;
+  }
+
+  .ability-scores {
+    display: grid;
+    justify-content: center;
+    margin: auto;
+    grid-template-columns: auto auto;
+    width: 10em;
+    margin-bottom: 1em;
+    column-gap: 2em;
+  }
+
+  .ability-scores div {
+    width: 5em;
   }
 </style>
