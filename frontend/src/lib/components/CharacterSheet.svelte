53e12839b7abb1d2e70a997f567620b9915e6807
rstacks <stacksrob910@gmail.com>
2025-05-08 00:27:36 -0500

    Made grid of equally sized tabs for looking at character info

--- frontend/src/lib/components/CharacterSheet.svelte
+++ frontend/src/lib/components/CharacterSheet.svelte
@@ -26,6 +26,51 @@
         <span>{character.xp}/{xpGoal} XP</span>
       </div>
     </header>
+    <div class="tabs five">
+      <input id="tab-1" type="radio" name="char-sheet-tabs" checked />
+      <input id="tab-2" type="radio" name="char-sheet-tabs" />
+      <input id="tab-3" type="radio" name="char-sheet-tabs" />
+      <input id="tab-4" type="radio" name="char-sheet-tabs" />
+      <input id="tab-5" type="radio" name="char-sheet-tabs" />
+      <div class="row">
+        <div>
+          fuck
+        </div>
+
+        <div>
+          shit
+        </div>
+
+        <div>
+          balls
+        </div>
+
+        <div>
+          cock
+        </div>
+
+        <div>
+          damn
+        </div>
+      </div>
+      <footer class="character-sheet-tab-buttons">
+        <div class="leftmost-tab">
+          <label for="tab-1">Overview</label>
+        </div>
+        <div>
+          <label for="tab-2">Weapons & Items</label>  
+        </div>
+        <div>
+          <label for="tab-3">Skills & Saves</label>  
+        </div>
+        <div>
+          <label for="tab-4">Spells</label>  
+        </div>
+        <div class="rightmost-tab">
+          <label for="tab-5">Stats</label>  
+        </div>
+      </footer>
+    </div>
   </article>
 </div>
 
@@ -61,5 +106,32 @@
 
   .close-button button img {
     width: 1.5em;
+  }
+
+  .character-sheet-tab-buttons {
+    width: 100%;
+    display: grid;
+    grid-template-columns: repeat(5, minmax(0, 1fr));
+    border-style: solid;
+    border-bottom: 0;
+    border-left: 0;
+    border-right: 0;
+    font-size: 0.75em;
+    padding: 0;
+  }
+
+  .character-sheet-tab-buttons div {
+    text-align: center;
+    border-style: solid;
+    border-top: 0;
+    border-bottom: 0;
+  }
+
+  .leftmost-tab {
+    border-left: 0;
+  }
+
+  .rightmost-tab {
+    border-right: 0;
   }
 </style>
