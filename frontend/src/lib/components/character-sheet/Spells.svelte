9a5559cd097de9a92374263549d4982d269abd88
rstacks <stacksrob910@gmail.com>
2025-05-10 14:50:34 -0500

    Finalized looks for spells tab on char sheet

--- frontend/src/lib/components/character-sheet/Spells.svelte
+++ frontend/src/lib/components/character-sheet/Spells.svelte
@@ -49,8 +49,99 @@
   </div>
 {/if}
 
+<div class="spells spell-list">
+  {#if props.lvl_1_spell_slots > 0}
+    <div class="feat-stat">
+      <strong>Level 1 Spell Slots:</strong>
+      <div>
+        <input class="xp-input" type="number" autocomplete="off"
+          value={props.lvl_1_spell_slots}>
+        <span>/ {props.lvl_1_spell_slots}</span>
+      </div>
+    </div>
+  {/if}
+  {#if props.lvl_2_spell_slots > 0}
+    <div class="feat-stat middle">
+      <strong>Level 2 Spell Slots:</strong>
+      <div>
+        <input class="xp-input" type="number" autocomplete="off"
+          value={props.lvl_2_spell_slots}>
+        <span>/ {props.lvl_2_spell_slots}</span>
+      </div>
+    </div>
+  {/if}
+  {#if props.lvl_3_spell_slots > 0}
+    <div class="feat-stat middle">
+      <strong>Level 3 Spell Slots:</strong>
+      <div>
+        <input class="xp-input" type="number" autocomplete="off"
+          value={props.lvl_3_spell_slots}>
+        <span>/ {props.lvl_3_spell_slots}</span>
+      </div>
+    </div>
+  {/if}
+  {#if props.lvl_4_spell_slots > 0}
+    <div class="feat-stat middle">
+      <strong>Level 4 Spell Slots:</strong>
+      <div>
+        <input class="xp-input" type="number" autocomplete="off"
+          value={props.lvl_4_spell_slots}>
+        <span>/ {props.lvl_4_spell_slots}</span>
+      </div>
+    </div>
+  {/if}
+
+  {#if props.rages > 0}
+    <div class="feat-stat">
+      <strong>Rage Uses:</strong>
+      <div>
+        <input class="xp-input" type="number" autocomplete="off"
+          value={props.rages}>
+        <span>/ {props.rages}</span>
+      </div>
+    </div>
+  {/if}
+  {#if props.rage_damage > 0}
+    <div class="feat-stat middle no-input">
+      <strong>Rage Damage:</strong>
+      <div>
+        <span>+{props.rage_damage}</span>
+      </div>
+    </div>
+  {/if}
+
+  {#if props.second_wind > 0}
+    <div class="feat-stat">
+      <strong>Second Wind Uses:</strong>
+      <div>
+        <input class="xp-input" type="number" autocomplete="off"
+          value={props.second_wind}>
+        <span>/ {props.second_wind}</span>
+      </div>
+    </div>
+  {/if}
+
+  {#if props.martial_arts}
+    <div class="feat-stat no-input">
+      <strong>Martial Arts Die:</strong>
+      <div>
+        <span>{props.martial_arts}</span>
+      </div>
+    </div>
+  {/if}
+
+  {#if props.sneak_attack}
+    <div class="feat-stat no-input">
+      <strong>Sneak Attack Dice:</strong>
+      <div>
+        <span>{props.sneak_attack}</span>
+      </div>
+    </div>
+  {/if}
+</div>
+
 {#if props.spells.length > 0}
-  <div class="spells">
+  <div class="spells spell-list">
     <strong>Spells</strong>
     <div class="spell-cards">
       {#each props.spells as spell}
@@ -103,5 +194,39 @@
   div.feat-header strong {
     display: flex;
     align-items: center;
+  }
+
+  .xp-input {
+    width: 4.5em;
+    margin-right: 0.25em;
+  }
+
+  div.feat-stat {
+    background-color: rgb(196, 230, 242);
+    border-radius: 0.5em;
+    padding: 0.5em;
+    padding-top: 0.25em;
+    padding-bottom: 0.25em;
+    display: flex;
+    align-items: center;
+  }
+
+  div.feat-stat div {
+    flex-grow: 1;
+    display: flex;
+    justify-content: end;
+    align-items: center;
+  }
+
+  .middle {
+    margin-top: 0.5em;
+  }
+
+  .no-input {
+    height: 2.5em;
+  }
+
+  div.spell-list {
+    margin-bottom: 1em;
   }
 </style>
