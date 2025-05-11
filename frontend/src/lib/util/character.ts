505de29fdcc0427c1eac58d705ad6de66a4105b5
rstacks <stacksrob910@gmail.com>
2025-05-11 14:57:00 -0500

    Renamed python script used to insert initial class levels

--- frontend/src/lib/util/character.ts
+++ frontend/src/lib/util/character.ts
@@ -107,10 +107,9 @@ export async function getSkills(): Promise<Skill[]> {
 }
 
 export function levelUp() {
-  // proficiency bonus goes up by one at level 5, 9, 13, 17
   // class specific stuff happens
-  // every level: +1 hit die (will be automatically shown, so don't change anything in the db)
+  // ability score increase every 4th level (lvl 4, 8, 12, 16)
+  //    max is 20 per ability score
   // increase max hp by average hit die roll result (ceil) + CON
   // Remember that when CON mod increases by 1, max HP increase by 1 per current level
-  // This should probably live in the backend
 }
